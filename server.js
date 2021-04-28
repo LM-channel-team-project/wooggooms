b7d4f940ae9eeb6b0ed559816e9549d7924f8df8
42KIM <ksahoo524@gmail.com>
2021-04-28 18:15:58 +0900

    second commit: project structure

--- server.js
+++ server.js
@@ -3,34 +3,61 @@ const fs = require('fs');
 const url = require('url');
 
 
-const app = http.createServer((req, res)=>{
+const app = http.createServer((req, res) => {
     let _url = req.url;
 
     if(_url === '/') {
-        fs.readFile('./views/main.html', (err, data)=> {
-            res.end(data);
+        fs.readFile('./views/main.html', (err, html) => {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.end(html);
         });
     } else if(_url === '/login') {
-        fs.readFile('./views/login.html', (err, data)=> {
-            res.end(data);
+        fs.readFile('./views/login.html', (err, html) => {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.end(html);
         });    
     } else if(_url === '/mypage') {
-        fs.readFile('./views/mypage.html', (err, data)=> {
-            res.end(data);
+        fs.readFile('./views/mypage.html', (err, html) => {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.end(html);
         });
     } else if(_url === '/create') {
-        fs.readFile('./views/create.html', (err, data)=> {
-            res.end(data);
+        fs.readFile('./views/create.html', (err, html) => {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.end(html);
         });
     } else if(_url === '/signup') {
-        fs.readFile('./views/signup.html', (err, data)=> {
-            res.end(data);
+        fs.readFile('./views/signup.html', (err, html) => {
+            res.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            res.end(html);
         });
+    } else if(_url === '/create_process') {
+        // process create-study
+        // redirect to mypage after processing create-study
+        res.writeHead(302, {Location: '/mypage'});
+        res.end();
+    } else if(_url === '/signup_process') {
+        // process sign-up
+        // redirect to mainpage after processing sign up
+        res.writeHead(302, {Location: '/'});
+        res.end();
+    } else {
+        res.writeHead(404);
+        res.end('Not found');
     }
-    console.log(_url);
 })
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
     console.log('listening on 3000');
 })
