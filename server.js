4f84dfb4d3a5986bcf682963a738afb86ab10c21
42KIM <ksahoo524@gmail.com>
2021-04-29 22:16:08 +0900

    Refactor: Set server.js basic structure

--- server.js
+++ server.js
@@ -1,63 +1,59 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
+
+const PORT = 3000;
 
 
 const app = http.createServer((req, res) => {
     let _url = req.url;
 
-    if(_url === '/') {
-        fs.readFile('./views/main.html', (err, html) => {
-            res.writeHead(200, {
-                "Content-Type": "text/html"
+    switch (_url) {
+        case '/':
+            fs.readFile('./views/main.html', (err, data) => {
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                res.end(data);
             });
-            res.end(html);
-        });
-    } else if(_url === '/login') {
-        fs.readFile('./views/login.html', (err, html) => {
-            res.writeHead(200, {
-                "Content-Type": "text/html"
+            break;
+        case '/sign-in':
+            fs.readFile('./views/sign-in.html', (err, data) => {
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                res.end(data);
             });
-            res.end(html);
-        });    
-    } else if(_url === '/mypage') {
-        fs.readFile('./views/mypage.html', (err, html) => {
-            res.writeHead(200, {
-                "Content-Type": "text/html"
+            break;
+        case '/mypage':
+            fs.readFile('./views/mypage.html', (err, data) => {
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                res.end(data);
             });
-            res.end(html);
-        });
-    } else if(_url === '/create') {
-        fs.readFile('./views/create.html', (err, html) => {
-            res.writeHead(200, {
-                "Content-Type": "text/html"
+            break;
+        case '/create':
+            fs.readFile('./views/create.html', (err, data) => {
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                res.end(data);
             });
-            res.end(html);
-        });
-    } else if(_url === '/signup') {
-        fs.readFile('./views/signup.html', (err, html) => {
-            res.writeHead(200, {
-                "Content-Type": "text/html"
+            break;
+        case '/sign-up':
+            fs.readFile('./views/sign-up.html', (err, data) => {
+                res.writeHead(200, {
+                    "Content-Type": "text/html"
+                });
+                res.end(data);
             });
-            res.end(html);
-        });
-    } else if(_url === '/create_process') {
-        // process create-study
-        // redirect to mypage after processing create-study
-        res.writeHead(302, {Location: '/mypage'});
-        res.end();
-    } else if(_url === '/signup_process') {
-        // process sign-up
-        // redirect to mainpage after processing sign up
-        res.writeHead(302, {Location: '/'});
-        res.end();
-    } else {
-        res.writeHead(404);
-        res.end('Not found');
+            break;
+        default:
+            res.writeHead(404);
+            res.end('Not Found');
     }
-})
+});
 
-
-app.listen(3000, function() {
-    console.log('listening on 3000');
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
 })
