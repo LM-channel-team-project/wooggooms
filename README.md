e36f241bff71818938df36952aaedefc2edca844
swparkdev <swpark.dev@gmail.com>
2021-05-01 18:31:52 +0900

    Setup express server and define routes

--- README.md
+++
@@ -1,5 +0,0 @@
-# (가제) 우리가 꿈꾸는 스터디
-### Members
-[loopbackseal](https://github.com/young961027)  
-[swparkdev](https://github.com/swparkdev)  
-[42KIM](https://github.com/42KIM)
--- server.js
+++ server.js
@@ -1,59 +1,64 @@
-const http = require('http');
-const fs = require('fs');
+const express = require("express");
+const app = express();
+const compression = require("compression");
+const helmet = require("helmet");
+app.use(compression());
+app.use(helmet());
 
 const PORT = 3000;
 
-
-const app = http.createServer((req, res) => {
-    let _url = req.url;
-
-    switch (_url) {
-        case '/':
-            fs.readFile('./views/main.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/sign-in':
-            fs.readFile('./views/sign-in.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/mypage':
-            fs.readFile('./views/mypage.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/create':
-            fs.readFile('./views/create.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/sign-up':
-            fs.readFile('./views/sign-up.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        default:
-            res.writeHead(404);
-            res.end('Not Found');
-    }
+// Main Route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views" + "/main.html");
 });
 
+// Sign-up Route
+app.get("/sign-up", (req, res) => {
+  res.sendFile(__dirname + "/views" + "/sign-up.html");
+});
+
+app.post("/sign-up_process", (req, res) => {
+  // Create DB table
+  res.redirect("/main");
+});
+
+// Sign-in Route
+app.get("/sign-in", (req, res) => {
+  res.sendFile(__dirname + "/views" + "/sign-in.html");
+});
+
+app.post("/sign-in_process", (req, res) => {
+  // Create DB table
+  res.redirect("/main");
+});
+
+// Mypage Route
+app.get("/mypage", (req, res) => {
+  res.sendFile(__dirname + "/views" + "/mypage.html");
+});
+
+// Create Route
+app.get("/create", (req, res) => {
+  res.sendFile(__dirname + "/views" + "/create.html");
+});
+
+app.post("/create_process", (req, res) => {
+  // Create DB table
+  res.redirect("/mypage");
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// 404 responses handler
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// Start HTTP server listening
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
-})
+  console.log(`listening on ${PORT}`);
+});
