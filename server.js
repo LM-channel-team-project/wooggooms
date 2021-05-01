eff47ec6d175884694e2fbc8c694fde6dac2d97a
42KIM <ksahoo524@gmail.com>
2021-05-01 17:45:03 +0900

    feat: Add Express framework on server.js

--- server.js
+++ server.js
@@ -1,59 +1,42 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const PORT = 3000;
 
+// Routes
+app.get('/', (req, res, next) => {
+    res.sendFile('main.html', { root: path.join(__dirname, 'views')}, (err) => {
+        if(err) {
+            next(err);
+        } else {
+            console.log("Sent: main.html");
+        }
+    });
+})
 
-const app = http.createServer((req, res) => {
-    let _url = req.url;
-
-    switch (_url) {
-        case '/':
-            fs.readFile('./views/main.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/sign-in':
-            fs.readFile('./views/sign-in.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/mypage':
-            fs.readFile('./views/mypage.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/create':
-            fs.readFile('./views/create.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        case '/sign-up':
-            fs.readFile('./views/sign-up.html', (err, data) => {
-                res.writeHead(200, {
-                    "Content-Type": "text/html"
-                });
-                res.end(data);
-            });
-            break;
-        default:
-            res.writeHead(404);
-            res.end('Not Found');
-    }
+app.get('/:pageName', (req, res, next) => {
+    const fileName = req.params.pageName;
+    res.sendFile(`${fileName}.html`, { root: path.join(__dirname, 'views')}, (err) => {
+        if(err) {
+            next(err);
+        } else {
+            console.log(`Sent: ${fileName}.html`);
+        }
+    });
 });
+
+
+// Handle 404 error
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+})
+// Handle server error
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
 
 app.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
-})
+});
