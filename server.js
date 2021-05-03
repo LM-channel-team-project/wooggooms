7b2ed2f0d9dae7f2fbbe46107ba4fe65887c8da9
loopbackseal <young961027@gmail.com>
2021-05-03 21:04:03 +0900

    refactor: Delete fileName, Change var to let, Change redirect /main to /

--- server.js
+++ server.js
@@ -6,37 +6,32 @@ app.use(helmet());
 
 const PORT = 3000;
 
-// 21.05.01 loopbackseal 수정내용:
-// 추후에 options 수정이 필요할 수 있기 때문에 options를 활용하여 sendFile 실행.
-//      -> view_options로 통일하여 선언. css의 경우 styles_option?
-//      -> 각 app.get() 익명함수 내부에 options를 선언한 것보다 느린것 같음. 의논 필요
-// 운영체제 별 차이로 인해 오류 발생 가능성 존재, path.join 활용.
-// 화살표 함수 -> 익명 함수로 수정.
-// ?: sign-up_process && sign-in_process 의 redirect -> "/main" or "/" 
+// 21.05.03 loopbackseal 수정내용:
+// fileName 변수 삭제하고 문자열 형태로 전달
+// var -> let로 수정.
+// sign-up_process && sign-in_process 의 redirect 수정 "/main" -> "/" 
 
-var views_options = {
+let views_options = {
     root : path.join(__dirname, "views")
 }
 // Main Route
 app.get("/", function(req, res, next) {
-    var fileName = "main";
-    res.sendFile(`${fileName}.html`, views_options, function(err) {
+    res.sendFile("main.html", views_options, function(err) {
         if(err) {
             next(err);
         } else {
-            console.log("Sent:",fileName);
+            console.log("Sent: main.html");
         }
     });
 });
 
 // Sign-up Route
 app.get("/sign-up", function(req, res, next) {
-    var fileName = "sign-up";
-    res.sendFile(`${fileName}.html`, views_options, function(err) {
+    res.sendFile("sign-up.html", views_options, function(err) {
         if(err) {
             next(err);
         } else {
-            console.log("Sent:",fileName);
+            console.log("Sent: sign-up.html");
         }
     });
 });
@@ -48,12 +43,11 @@ app.post("/sign-up_process", function(req, res) {
 
 // Sign-in Route
 app.get("/sign-in", function(req, res, next) {
-    var fileName = "sign-in";
-    res.sendFile(`${fileName}.html`, views_options, function(err) {
+    res.sendFile("sign-in.html", views_options, function(err) {
         if(err) {
             next(err);
         } else {
-            console.log("Sent:",fileName);
+            console.log("Sent: sign-in.html");
         }
     });
 });
@@ -65,24 +59,22 @@ app.post("/sign-in_process", function(req, res) {
 
 // Mypage Route
 app.get("/mypage", function(req, res, next) {
-    var fileName = "mypage";
-    res.sendFile(`${fileName}.html`, views_options, function(err) {
+    res.sendFile("mypage.html", views_options, function(err) {
         if(err) {
             next(err);
         } else {
-            console.log("Sent:",fileName);
+            console.log("Sent: mypage.html");
         }
     });
 });
 
 // Create Route
 app.get("/create", function(req, res, next) {
-    var fileName = "create";
-    res.sendFile(`${fileName}.html`, views_options, function(err) {
+    res.sendFile("create.html", views_options, function(err) {
         if(err) {
             next(err);
         } else {
-            console.log("Sent:",fileName);
+            console.log("Sent: create.html");
         }
     });
 });
