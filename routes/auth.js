d2fca0519dfb40f7fdb8c13e6919971bac5ae9f5
42KIM <ksahoo524@gmail.com>
2021-05-13 15:58:51 +0900

    Add passport-facebook-oauth

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,13 @@ const { nanoid } = require('nanoid');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const views_options = {
   root: path.join(__dirname, '../views')
 };
 const dbConfig = require('../config/databaseConfig');
 const { db } = dbConfig;
+const dotenv = require('dotenv').config();
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
@@ -55,13 +57,12 @@ passport.use(
 );
 
 // Passport GoogleOAuth Strategy
-const googleCredentials = require('../config/google.json');
 passport.use(
   new GoogleStrategy(
     {
-      clientID: googleCredentials.web.client_id,
-      clientSecret: googleCredentials.web.client_secret,
-      callbackURL: googleCredentials.web.redirect_uris[0]
+      clientID: process.env.GG_ID,
+      clientSecret: process.env.GG_SECRET,
+      callbackURL: process.env.GG_CBURL
     },
     function (accessToken, refreshToken, profile, done) {
       const sns_id = profile.id;
@@ -102,6 +103,65 @@ passport.use(
       });
     }
   )
+);
+
+// Passport Facebook Strategy
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FB_ID,
+      clientSecret: process.env.FB_SECRET,
+      callbackURL: process.env.FB_CBURL
+    },
+    function (accessToken, refreshToken, profile, done) {
+      const sns_id = profile.id;
+      const sns_type = profile.provider;
+      const sql = 'SELECT * FROM user WHERE sns_id=? AND sns_type=?';
+      db.query(sql, [sns_id, sns_type], function (err, results) {
+        const user = results[0];
+        // 처음 방문한 유저라면: 회원가입 시키고 로그인 시키기
+        if (!user) {
+          const id = nanoid();
+          const nickname = profile.displayName;
+          const sql =
+            'INSERT INTO user (id, sns_id, sns_type, nickname) VALUES (?, ?, ?, ?)';
+          db.query(
+            sql,
+            [id, sns_id, sns_type, nickname],
+            function (err, result, field) {
+              if (err) {
+                console.log(err);
+              }
+              console.log('페이스북 계정으로 처음 방문하셨네요!');
+              const sql = 'SELECT * FROM user WHERE sns_id=? AND sns_type=?';
+              db.query(sql, [sns_id, sns_type], function (err, results) {
+                if (err) {
+                  console.log(err);
+                }
+                console.log('페이스북 계정으로 가입시켰습니다!');
+                return done(null, results[0]);
+              });
+            }
+          );
+        }
+        // 처음 방문한 유저가 아니라면: 바로 로그인 시키기
+        if (user) {
+          console.log('재방문 하셨네요!');
+          return done(null, user);
+        }
+      });
+    }
+  )
+);
+
+router.get('/facebook', passport.authenticate('facebook'));
+
+router.get(
+  '/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/auth/sign-in'
+  })
 );
 
 router.get(
