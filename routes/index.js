4637317c90ccc841d0fdb8d8a8ad813dd884e08e
loopbackseal <young961027@gmail.com>
2021-05-20 18:38:44 +0900

    Feat : Add classification with ejs && css

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,27 @@
+/* eslint-disable camelcase */
 const express = require('express');
 
 const router = express.Router();
-const path = require('path');
-
-const views_options = {
-  root: path.join(__dirname, '../views')
-};
 
 // Main Route
+// eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
-  res.sendFile('main.html', views_options, err => {
-    if (err) {
-      next(err);
-    } else {
-      console.log('Sent: main.html');
-    }
+  res.render('main.ejs', {
+    mainCategory: ['추천', '전체', '공무원', '어학', '취업', '수능', '취미'],
+    middleCategory: [
+      ['9급', '7급', '5급'],
+      ['영어', '중국어', '일본어', '기타'],
+      [
+        '경영/사무',
+        '마케팅',
+        '반도체',
+        'IT/인터넷',
+        '디자인',
+        '미디어',
+        '기타'
+      ],
+      ['독서', '토론']
+    ]
   });
 });
 module.exports = router;
