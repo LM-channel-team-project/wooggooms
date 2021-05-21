a496b400df7e6e2a77b7d39c75ed850d73b5fdc6
swparkdev <swpark.dev@gmail.com>
2021-05-22 05:19:04 +0900

    Update group list component to 1.0.1
    - Change structure in group-list.ejs
    - Make minor layout changes
    - Delete unnecessary css variables
    - Change main.ejs to index.ejs

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
     if (err) {
       next(err);
     } else {
-      res.render('main', { data: results });
+      res.render('index', { data: results });
     }
   });
 });
