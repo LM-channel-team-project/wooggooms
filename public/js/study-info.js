a728656d65657f6e030f600153601a06e12d2102
sanoopark <sanoopark@gmail.com>
2021-05-31 18:14:19 +0900

    fix: Update study-info 1.0.3
    - Modify DOM tree
    - Modify BEM class name

--- public/js/study-info.js
+++ public/js/study-info.js
@@ -2,19 +2,5 @@ function goBack() {
   window.history.back();
 }
 
-function openForm() {
-  document.getElementById('chatPopUp').style.display = 'block';
-}
-
-function closeForm() {
-  document.getElementById('chatPopUp').style.display = 'none';
-}
-
 const backBtn = document.querySelector('.study-info__back-btn');
 backBtn.addEventListener('click', goBack);
-
-const openBtn = document.querySelector('.study-info__chat-btn');
-openBtn.addEventListener('click', openForm);
-
-const closeBtn = document.querySelector('.chat-popup__cncl-btn');
-closeBtn.addEventListener('click', closeForm);
