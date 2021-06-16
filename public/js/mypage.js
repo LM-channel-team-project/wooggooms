0c075bcd355991f78d47b71babf03520ffb4c989
42KIM <ksahoo524@gmail.com>
2021-06-16 16:20:37 +0900

    feat: Add quit modal

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -1,26 +1,46 @@
 const myinfo_btn = document.querySelector(".my-info__btn");
-const delete_modal = document.querySelector(".delete-modal");
-const delete_btn = document.querySelectorAll(".group-list__del-btn");
-const no_btn = document.querySelector(".delete-modal__no-btn");
+const quit_modal = document.querySelector(".quit-modal");
+const quit_btn = document.querySelectorAll(".group-list__quit-btn");
+const quit_no_btn = document.querySelectorAll(".quit-modal__no-btn");
+const del_modal = document.querySelector(".del-modal");
+const del_btn = document.querySelectorAll(".group-list__del-btn");
+const del_no_btn = document.querySelectorAll(".del-modal__no-btn");
+
 
 function redirectEditPage() {
     location.href = "http://localhost:3000/mypage/edit-myinfo";
 }
 
-function showModal() {
-    delete_modal.classList.add("show-modal");
+function openQuitModal() {
+    quit_modal.classList.add("show-modal");
 }
 
-function closeModal() {
-    delete_modal.classList.remove("show-modal");
+function closeQuitModal() {
+    quit_modal.classList.remove("show-modal");
+}
+
+function openDelModal() {
+    del_modal.classList.add("show-modal");
+}
+
+function closeDelModal() {
+    del_modal.classList.remove("show-modal");
 }
 
 function init() {
     myinfo_btn.addEventListener("click", redirectEditPage);
-    delete_btn.forEach(Item => {
-        Item.addEventListener("click", showModal);
-    })
-    no_btn.addEventListener("click", closeModal);
+    quit_btn.forEach(Item => {
+        Item.addEventListener("click", openQuitModal);
+    });
+    quit_no_btn.forEach(Item => {
+        Item.addEventListener("click", closeQuitModal);
+    });
+    del_btn.forEach(Item => {
+        Item.addEventListener("click", openDelModal);
+    });
+    del_no_btn.forEach(Item => {
+        Item.addEventListener("click", closeDelModal);
+    });
 }
 
 init();
