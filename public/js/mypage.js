a4082dc6b7e4cd7345dc4064444b445da94efc96
42KIM <ksahoo524@gmail.com>
2021-06-16 20:45:20 +0900

    refactor: Modify openModal/closeModal function

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -1,46 +1,36 @@
 const myinfo_btn = document.querySelector(".my-info__btn");
 const quit_modal = document.querySelector(".quit-modal");
 const quit_btn = document.querySelectorAll(".group-list__quit-btn");
-const quit_no_btn = document.querySelectorAll(".quit-modal__no-btn");
 const del_modal = document.querySelector(".del-modal");
 const del_btn = document.querySelectorAll(".group-list__del-btn");
-const del_no_btn = document.querySelectorAll(".del-modal__no-btn");
-
+const no_btn = document.querySelectorAll(".modal__no-btn");
 
 function redirectEditPage() {
     location.href = "http://localhost:3000/mypage/edit-myinfo";
 }
 
-function openQuitModal() {
-    quit_modal.classList.add("show-modal");
+function openModal(modal) {
+    modal.classList.add("show-modal");
 }
 
-function closeQuitModal() {
-    quit_modal.classList.remove("show-modal");
-}
-
-function openDelModal() {
-    del_modal.classList.add("show-modal");
-}
-
-function closeDelModal() {
-    del_modal.classList.remove("show-modal");
+function closeModal(clicked) {
+    const cur_modal = clicked.target.parentNode.parentNode.parentNode;
+    cur_modal.classList.remove("show-modal");
 }
 
 function init() {
     myinfo_btn.addEventListener("click", redirectEditPage);
     quit_btn.forEach(Item => {
-        Item.addEventListener("click", openQuitModal);
+        Item.addEventListener("click", () => openModal(quit_modal));
     });
-    quit_no_btn.forEach(Item => {
-        Item.addEventListener("click", closeQuitModal);
-    });
+
     del_btn.forEach(Item => {
-        Item.addEventListener("click", openDelModal);
+        Item.addEventListener("click", () => openModal(del_modal));
     });
-    del_no_btn.forEach(Item => {
-        Item.addEventListener("click", closeDelModal);
-    });
+
+    no_btn.forEach(Item => {
+        Item.addEventListener("click", closeModal)
+    })
 }
 
 init();
