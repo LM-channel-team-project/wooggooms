06842ba18098630fc795808b5dfbd2c839528ae4
loopbackseal <young961027@gmail.com>
2021-07-20 16:42:22 +0900

    feat: Add event listener to group enter button

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -1,36 +1,45 @@
-const myinfo_btn = document.querySelector(".my-info__btn");
-const quit_modal = document.querySelector(".quit-modal");
-const quit_btn = document.querySelectorAll(".group-list__quit-btn");
-const del_modal = document.querySelector(".del-modal");
-const del_btn = document.querySelectorAll(".group-list__del-btn");
-const no_btn = document.querySelectorAll(".modal__no-btn");
+const myinfo_btn = document.querySelector('.my-info__btn');
+const quit_modal = document.querySelector('.quit-modal');
+const quit_btn = document.querySelectorAll('.group-list__quit-btn');
+const del_modal = document.querySelector('.del-modal');
+const del_btn = document.querySelectorAll('.group-list__del-btn');
+const no_btn = document.querySelectorAll('.modal__no-btn');
+const group_enter_btn = document.querySelectorAll('.group-list__enter-btn');
 
 function redirectEditPage() {
-    location.href = "http://localhost:3000/mypage/edit-myinfo";
+  location.href = 'http://localhost:3000/mypage/edit-myinfo';
+}
+function redeirectGroupEditPage() {
+  location.href = 'http://localhost:3000/mypage/group-edit';
 }
 
 function openModal(modal) {
-    modal.classList.add("show-modal");
+  modal.classList.add('show-modal');
 }
 
 function closeModal() {
-    const cur_modal = this.closest(".show-modal");
-    cur_modal.classList.remove("show-modal");
+  const cur_modal = this.closest('.show-modal');
+  cur_modal.classList.remove('show-modal');
 }
 
 function init() {
-    myinfo_btn.addEventListener("click", redirectEditPage);
-    quit_btn.forEach(Item => {
-        Item.addEventListener("click", () => openModal(quit_modal));
-    });
+  myinfo_btn.addEventListener('click', redirectEditPage);
+  quit_btn.forEach(Item => {
+    Item.addEventListener('click', () => openModal(quit_modal));
+  });
 
-    del_btn.forEach(Item => {
-        Item.addEventListener("click", () => openModal(del_modal));
-    });
+  del_btn.forEach(Item => {
+    Item.addEventListener('click', () => openModal(del_modal));
+  });
 
-    no_btn.forEach(Item => {
-        Item.addEventListener("click", closeModal)
-    })
+  no_btn.forEach(Item => {
+    Item.addEventListener('click', closeModal);
+  });
+
+  //   임시적으로는 그냥 group-edit page로 redirect하지만, 추후에는 클릭된 그룹의 id도 함께 넘겨주어야 할듯
+  group_enter_btn.forEach(Item => {
+    Item.addEventListener('click', redeirectGroupEditPage);
+  });
 }
 
 init();
