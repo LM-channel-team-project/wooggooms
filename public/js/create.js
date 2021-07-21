f9f781087cb373ce39c0ecc69ff338f14974bcf6
42KIM <75300807+42KIM@users.noreply.github.com>
2021-07-21 22:42:40 +0900

    fix: Fix conflic

--- public/js/create.js
+++ public/js/create.js
@@ -1,36 +1,60 @@
-const mainBtn = document.querySelectorAll('.cat-filter-main__btn');
-const subBtn = document.querySelectorAll('.cat-filter-sub__btn');
-const mainInput = document.querySelector('.group-create-category__input-main');
-const subInput = document.querySelector('.group-create-category__input-sub');
+const backBtn = document.querySelector('.group-create-title__back-btn');
+const mainCatBtns = document.querySelectorAll('.cat-filter-main__btn');
+const subCatBtns = document.querySelectorAll('.cat-filter-sub__btn');
+const mainCatInput = document.querySelector('.group-create-category__input-main');
+const subCatInput = document.querySelector('.group-create-category__input-sub');
 
-function setSubInput(e) {
-  const text = e.target.textContent;
-  console.log(text);
-  subInput.setAttribute('value', text);
+function goBack() {
+    window.history.back();
 }
-function setMainInput(e) {
-  const mainCategory = e.target.textContent;
-  const cat_name = e.target.classList[1];
-  const selector = 'div.cat-filter-sub button.'.concat(cat_name);
-  const subCategory = document.querySelector(selector);
-  //   subCategory.handleSub(e);
-  const pre_sub = document.querySelector('.active-sub');
-  if (pre_sub !== null) pre_sub.classList.remove('active-sub');
-  subCategory.classList.add('active-sub');
-  console.log(mainCategory, subCategory);
-  mainInput.setAttribute('value', mainCategory);
-  subInput.setAttribute('value', subCategory.textContent);
+
+function setInput() {
+    const text = this.textContent;
+    const parentClass = this.parentNode.getAttribute('class');
+    if(parentClass === 'cat-filter-main') {
+        mainCatInput.value = text;
+        subCatInput.value = '';
+    } else {
+        subCatInput.value = text;
+    }
 }
+
+// const group_name = document.querySelector('.group-create-name__input');
+// const group_loc = document.querySelector('.group-create-condition select[name=location]');
+// const group_gen = document.querySelector('.group-create-condition select[name=gender]');
+// const group_mem = document.querySelector('.group-create-condition select[name=members]');
+// const group_desc = document.querySelector('.group-create-intro__text');
+// const submit_btn = document.querySelector('.group-create-submit__btn');
+// function checkValid() {
+//     let valid_name = false;
+//     let valid_loc = false;
+//     let valid_gen = false;
+//     let valid_mem = false;
+//     let valid_cat = false;
+//     let vaild_desc = false;
+//     if(group_name.value) valid_name = true;
+//     if(group_loc.value) valid_loc = true;
+//     if(group_gen.value) valid_gen = true;
+//     if(group_mem.value) valid_mem = true;
+//     if(input_sub.value) valid_cat = true;
+//     if(group_desc.value) vaild_desc = true;
+//     if(valid_name && valid_loc && valid_gen && valid_mem && valid_cat && vaild_desc) submit_btn.removeAttribute("disabled");
+//     else submit_btn.setAttribute("disabled", true);
+// }
 
 function init() {
-  mainBtn.forEach(Item => {
-    Item.type = 'button';
-    Item.addEventListener('click', setMainInput);
-  });
-  subBtn.forEach(Item => {
-    Item.type = 'button';
-    Item.addEventListener('click', setSubInput);
-  });
+    backBtn.addEventListener('click', goBack);
+    mainCatBtns.forEach(Item => {
+        Item.type = 'button';
+        Item.addEventListener('click', setInput);
+    });
+    subCatBtns.forEach(Item => {
+        Item.type = 'button';
+        Item.addEventListener('click', setInput);
+    });
+    // mainCatBtns.forEach(Item => Item.addEventListener('click', checkValid));
+    // subCatBtns.forEach(Item => Item.addEventListener('click', checkValid));
+    // document.addEventListener('input', checkValid);
 }
 
 init();
