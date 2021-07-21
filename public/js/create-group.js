9273818b53f6c02ffd0d05feb8333ded12d7f8c4
42KIM <ksahoo524@gmail.com>
2021-07-21 19:42:56 +0900

    feat: '스터디 생성(create_process)' 기능 구현

---
+++ public/js/create-group.js
@@ -0,0 +1,62 @@
+const backBtn = document.querySelector('.group-create-title__back-btn');
+const mainCatBtns = document.querySelectorAll('.cat-filter-main__btn');
+const subCatBtns = document.querySelectorAll('.cat-filter-sub__btn');
+const mainCatInput = document.querySelector('.group-create-category__input-main');
+const subCatInput = document.querySelector('.group-create-category__input-sub');
+
+function goBack() {
+    window.history.back();
+}
+
+// 카테고리를 선택하면 hidden input에 해당 값을 세팅해주는 함수
+function setInput() {
+    const text = this.textContent;
+    const parentClass = this.parentNode.getAttribute('class');
+    if(parentClass === "cat-filter-main") {
+        mainCatInput.setAttribute('value', text);
+        subCatInput.setAttribute('value', '');
+    } else {
+        subCatInput.setAttribute('value', text);
+    }
+}
+
+// const group_name = document.querySelector('.group-create-name__input');
+// const group_loc = document.querySelector('.group-create-condition select[name=location]');
+// const group_gen = document.querySelector('.group-create-condition select[name=gender]');
+// const group_mem = document.querySelector('.group-create-condition select[name=members]');
+// const group_desc = document.querySelector('.group-create-intro__text');
+// const submit_btn = document.querySelector('.group-create-submit__btn');
+// validCheck 일단 보류
+// function checkValid() {
+//     let valid_name = false;
+//     let valid_loc = false;
+//     let valid_gen = false;
+//     let valid_mem = false;
+//     let valid_cat = false;
+//     let vaild_desc = false;
+//     if(group_name.value) valid_name = true;
+//     if(group_loc.value) valid_loc = true;
+//     if(group_gen.value) valid_gen = true;
+//     if(group_mem.value) valid_mem = true;
+//     if(input_sub.value) valid_cat = true;
+//     if(group_desc.value) vaild_desc = true;
+//     if(valid_name && valid_loc && valid_gen && valid_mem && valid_cat && vaild_desc) submit_btn.removeAttribute("disabled");
+//     else submit_btn.setAttribute("disabled", true);
+// }
+
+function init() {
+    backBtn.addEventListener('click', goBack);
+    mainCatBtns.forEach(Item => {
+        Item.type = 'button';
+        Item.addEventListener('click', setInput);
+    });
+    subCatBtns.forEach(Item => {
+        Item.type = 'button';
+        Item.addEventListener('click', setInput);
+    });
+    // mainCatBtns.forEach(Item => Item.addEventListener('click', checkValid));
+    // subCatBtns.forEach(Item => Item.addEventListener('click', checkValid));
+    // document.addEventListener('input', checkValid);
+}
+
+init();
