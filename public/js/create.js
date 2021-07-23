4bd616ef9475248970e21d12645364e51f72e42e
ksh <ksahoo524@gmail.com>
2021-07-23 11:35:24 +0900

    refactor: Modify checkValid func, add preventSubmit

--- public/js/create.js
+++ public/js/create.js
@@ -7,6 +7,22 @@ const mainCatInput = document.querySelector(
 );
 const subCatInput = document.querySelector('.group-create-category__input-sub');
 const createBtn = document.querySelector('.group-create-submit__btn');
+const groupName = document.querySelector('.group-create-name__input');
+const groupLoc = document.querySelector(
+  '.group-create-condition select[name=location]'
+);
+const groupGen = document.querySelector(
+  '.group-create-condition select[name=gender]'
+);
+const groupMem = document.querySelector(
+  '.group-create-condition select[name=members]'
+);
+const groupDesc = document.querySelector('.group-create-intro__text');
+const submitBtn = document.querySelector('.group-create-submit__btn');
+
+function preventSubmit(e) {
+  if (e.keyCode === 13) e.preventDefault();
+}
 
 function goBack() {
   window.history.back();
@@ -23,48 +39,27 @@ function setInput() {
   }
 }
 
-const group_name = document.querySelector('.group-create-name__input');
-const group_loc = document.querySelector(
-  '.group-create-condition select[name=location]'
-);
-const group_gen = document.querySelector(
-  '.group-create-condition select[name=gender]'
-);
-const group_mem = document.querySelector(
-  '.group-create-condition select[name=members]'
-);
-const group_desc = document.querySelector('.group-create-intro__text');
-const submit_btn = document.querySelector('.group-create-submit__btn');
 function checkValid() {
-  let valid_name = false;
-  let valid_loc = false;
-  let valid_gen = false;
-  let valid_mem = false;
-  let valid_cat = false;
-  let vaild_desc = false;
-  if (group_name.value) valid_name = true;
-  if (group_loc.value) valid_loc = true;
-  if (group_gen.value) valid_gen = true;
-  if (group_mem.value) valid_mem = true;
-  if (subCatInput.value) valid_cat = true;
-  if (group_desc.value) vaild_desc = true;
   if (
-    valid_name &&
-    valid_loc &&
-    valid_gen &&
-    valid_mem &&
-    valid_cat &&
-    vaild_desc
-  )
+    groupName.value &&
+    groupLoc.value &&
+    groupGen.value &&
+    groupMem.value &&
+    subCatInput.value &&
+    groupDesc.value
+  ) {
     return true;
+  }
+  return false;
 }
 
 function submit() {
   if (checkValid()) form.submit();
-  else alert('모든 항목을 작성해주세요!');
+  else alert('작성되지 않은 항목이 있습니다!');
 }
 
 function init() {
+  groupName.addEventListener('keydown', preventSubmit);
   backBtn.addEventListener('click', goBack);
   mainCatBtns.forEach(Item => {
     Item.type = 'button';
