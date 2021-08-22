8ea0e9e96f92b3ffe32d5f7416e2f7d7088de94f
Sangwoo Park <sanoopark@gmail.com>
2021-08-22 23:09:10 +0900

    feat: 회원가입 프론트 유효성 검사

---
+++ public/js/sign-up.js
@@ -0,0 +1,95 @@
+const form = document.querySelector('.sign-up-form');
+const emailInput = document.querySelector('.sign-up-form__input[name=email]');
+const pwdInput = document.querySelector('.sign-up-form__input[name=pwd]');
+const pwd2Input = document.querySelector('.sign-up-form__input[name=pwd2]');
+const nameInput = document.querySelector('.sign-up-form__input[name=nickname]');
+const submitBtn = document.querySelector('.sign-up-form__submit');
+
+const emailValidDiv = document.querySelector('.sign-up-form__email');
+const pwdValidDiv = document.querySelector('.sign-up-form__pwd');
+const pwd2ValidDiv = document.querySelector('.sign-up-form__pwd2');
+const nameValidDiv = document.querySelector('.sign-up-form__name');
+
+function showIsValid(element, text) {
+  element.innerText = text;
+}
+
+function checkValidEmail() {
+  const regExp =
+    /^[\w!#$%&'*+/=?^_{|}~-]+(?:\.[\w!#$%&'*+/=?^_{|}~-]+)*@(?:\w+\.)+\w+$/;
+
+  if (emailInput.value === '' || regExp.test(emailInput.value)) {
+    showIsValid(emailValidDiv, '');
+    return true;
+  } else {
+    showIsValid(emailValidDiv, '올바른 이메일 주소를 입력해 주세요.');
+    return false;
+  }
+}
+
+function checkStrongPwd() {
+  const regExp =
+    /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]).{8,16}$/;
+
+  if (pwdInput.value === '' || regExp.test(pwdInput.value)) {
+    showIsValid(pwdValidDiv, '');
+    return true;
+  } else {
+    showIsValid(
+      pwdValidDiv,
+      '8~16자 영문 대 소문자, 숫자, 특수문자를 사용해 주세요.'
+    );
+    return false;
+  }
+}
+
+function checkEqualPwd() {
+  if (pwd2Input.value !== '' && pwdInput.value === pwd2Input.value) {
+    showIsValid(pwd2ValidDiv, '');
+    return true;
+  } else {
+    showIsValid(pwd2ValidDiv, '비밀번호가 일치하지 않습니다.');
+    return false;
+  }
+}
+
+function checkValidName() {
+  if (nameInput.value === '' || nameInput.value.length <= 10) {
+    showIsValid(nameValidDiv, '');
+    return true;
+  } else {
+    showIsValid(nameValidDiv, '10자 이하를 입력해 주세요.');
+    return false;
+  }
+}
+
+function checkValidAll() {
+  const inputs = [emailInput, pwdInput, pwd2Input, nameInput];
+  const divs = [emailValidDiv, pwdValidDiv, pwd2ValidDiv, nameValidDiv];
+
+  inputs.forEach((Item, i) => {
+    if (!Item.value) {
+      showIsValid(divs[i], '필수 정보입니다.');
+    }
+  });
+
+  if (
+    checkValidEmail() &&
+    checkStrongPwd() &&
+    checkEqualPwd() &&
+    checkValidName()
+  ) {
+    form.submit();
+  }
+}
+
+function init() {
+  emailInput.addEventListener('keyup', checkValidEmail);
+  pwdInput.addEventListener('keyup', checkStrongPwd);
+  pwdInput.addEventListener('keyup', checkEqualPwd);
+  pwd2Input.addEventListener('keyup', checkEqualPwd);
+  nameInput.addEventListener('keyup', checkValidName);
+  submitBtn.addEventListener('click', checkValidAll);
+}
+
+init();
