3dd1ba10063c9fbd2646219d22e5e722ebd0cb38
loopbackseal <young961027@gmail.com>
2021-08-30 14:14:02 +0900

    refactor : group-edit.js 가독성 개선
    각각의 중분류 배열 선언 대신, subCatList 객체를 생성, for in 을 통한 중분류 분배 대신 for of 사용

--- public/js/group-edit.js
+++ public/js/group-edit.js
@@ -12,27 +12,29 @@ const kickoutBtnAll = document.querySelectorAll(
 const checkForm = document.querySelector('.name-check-form');
 const checkBtn = document.querySelector('.name-check-btn');
 const nameInput = document.querySelector('.group-edit-name__input');
-// 얘네도 바뀔때마다 받아오거나 수정해야 할 것 같다
-const office = ['9급', '7급', '5급'];
-const lang = ['영어', '중국어', '일본어'];
-const employ = ['경영/사무', 'IT/인터넷', '마케팅', '디자인', '미디어'];
-const hobby = ['독서', '토론', '기타'];
-const exam = ['고1', '고2', '고3', 'N수'];
+
+const subCatList = {
+  office: ['9급', '7급', '5급'],
+  lang: ['영어', '중국어', '일본어'],
+  employ: ['경영/사무', 'IT/인터넷', '마케팅', '디자인', '미디어'],
+  hobby: ['독서', '토론', '기타'],
+  exam: ['고1', '고2', '고3', 'N수']
+};
 
 function setSub(mainValue) {
   let val = ['전체'];
-  if (mainValue === '공무원') val = office;
-  else if (mainValue === '어학') val = lang;
-  else if (mainValue === '취업') val = employ;
-  else if (mainValue === '취미') val = hobby;
-  else if (mainValue === '수능') val = exam;
+  if (mainValue === '공무원') val = subCatList.office;
+  else if (mainValue === '어학') val = subCatList.lang;
+  else if (mainValue === '취업') val = subCatList.employ;
+  else if (mainValue === '취미') val = subCatList.hobby;
+  else if (mainValue === '수능') val = subCatList.exam;
 
   cat_sub.options.length = 0;
-  for (let x in val) {
+  for (const sub_category of val) {
     let opt = document.createElement('option');
-    opt.value = val[x];
-    opt.textContent = val[x];
-    if (sub_value === val[x]) opt.selected = true;
+    opt.value = sub_category;
+    opt.textContent = sub_category;
+    if (sub_value === sub_category) opt.selected = true;
     cat_sub.appendChild(opt);
   }
 }
